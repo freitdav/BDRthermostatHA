03851cefa3d63e6e9445f5a55c3e664c4606bf82
Richard <80462098+055Richard@users.noreply.github.com>
2022-10-06 22:28:34 +0200

    Energy added. Not yet tested.

--- custom_components/bdr_thermostat/sensor.py
+++ custom_components/bdr_thermostat/sensor.py
@@ -43,7 +43,8 @@ async def async_setup_platform(
             ErrorSensor(hass, config), 
             FlowTemperatureSensor(hass, config),
 			OutsideTemperatureSensor(hass, config),
-			HeatingSensor(hass, config)
+			HeatingSensor(hass, config),
+			EnergyConsumptionSensor(hass, config)
         ],
         update_before_add=True,
     )
@@ -57,7 +58,8 @@ async def async_setup_entry(hass, config_entry, async_add_devices):
             ErrorSensor(hass, config_entry.data),
             FlowTemperatureSensor(hass, config_entry.data),
 			OutsideTemperatureSensor(hass, config_entry.data),
-			HeatingSensor(hass, config_entry.data)
+			HeatingSensor(hass, config_entry.data),
+			EnergyConsumptionSensor(hass, config_entry.data)
         ],
         update_before_add=True,
     )     
@@ -101,6 +103,7 @@ class WaterPressureSensor(SensorEntity):
             self._attr_native_value = "N/A"    
 
 
+
 class ErrorSensor(SensorEntity):
     
     def __init__(self, hass, config):
@@ -137,6 +140,46 @@ class ErrorSensor(SensorEntity):
         else:
             #self._attr_native_unit_of_measurement = ""
             self._attr_native_value = "N/A"    
+
+
+class EnergyConsumptionSensor(SensorEntity):
+    
+    def __init__(self, hass, config):
+        """Initialize the sensor."""
+        super().__init__()
+        self.hass = hass
+        self._bdr_api = hass.data[PLATFORM].get(DATA_KEY_API)
+        self._attr_device_class = SensorDeviceClass.ENERGY
+        self._attr_state_class = SensorStateClass.TOTAL_INCREASING
+        self._attr_should_poll = True
+        self._attr_device_info = {
+            "identifiers": {
+                (
+                    SERIAL_KEY,
+                    self._bdr_api.get_device_information().get("serial", "1234"),
+                )
+            }
+		}
+        self._attr_name = config.get(CONF_NAME) + " Heating consumption"
+        self._attr_unique_id = self._attr_name
+
+    @property
+    def available(self) -> bool:
+        """Return True if entity is available."""
+        return self._bdr_api.is_bootstraped()
+
+    async def async_update(self):
+    
+        consumptions = await self._bdr_api.get_consumptions()
+        consumption = consumptions.get("energyCH", None)
+
+        if consumption:
+            self._attr_native_value = int(consumption["value"])             
+            self._attr_native_unit_of_measurement = consumption["unit"]     
+
+        else:
+            self._attr_native_unit_of_measurement = ""
+            self._attr_native_value = "N/A"   
 
 class FlowTemperatureSensor(SensorEntity):
     
